eb4bfafabbb3f2336d17bc2e2454fcb1b3e11905
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-04 19:41:22 +0100

    Finish day 1

---
+++ day-001/band-name.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band name Generator")
+
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+print("Your band name could be " + city + " " + pet_name)
