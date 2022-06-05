554c2723febcff60c365f02eb1f41a71dc039847
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-05 13:52:04 +0100

    Add helpful funcs

--- day-002/primitive-data-types.py
+++ day-002/primitive-data-types.py
@@ -11,3 +11,12 @@ print(3.14159)
 # Boolean
 # Always begin with a capital - True/False (reasons!)
 print(True)
+
+# Find data type
+print(type(1))
+print(type(1.1))
+print(type("bla"))
+
+# Type conversion
+print(type(str(12345)))
+print(type(float(12345)))
