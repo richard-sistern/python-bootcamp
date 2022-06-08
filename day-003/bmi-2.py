aa6ae5851c928595dc7ca9758d5a3d63258ab716
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-08 21:13:06 +0100

    Update BMI

---
+++ day-003/bmi-2.py
@@ -0,0 +1,19 @@
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+bmi = round(weight / height ** 2)
+
+if bmi <= 18.5:
+    advice = "underweight"
+elif bmi > 18.5 and bmi <= 25:
+    advice = "normal weight"
+elif bmi > 25 and bmi <=30:
+    advice = "overweight"
+elif bmi > 30 and bmi <= 35:
+    advice = "obese"
+else:
+    advice = "clinically obese"
+
+
+
+print(f"Your BMI is {bmi}, you are {advice}")
