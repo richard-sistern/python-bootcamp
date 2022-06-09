cf3663b33c8581c6c78ed3aa1fc91d2473ee7220
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-09 15:53:35 +0100

    Add example

--- day-003/leap-year.py
+++ day-003/leap-year.py
@@ -9,3 +9,15 @@ if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
     print("Leap year")
 else:
     print("Not a leap year")
+
+# Example solution
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not a leap year")
