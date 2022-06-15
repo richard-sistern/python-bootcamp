56333903a755e76306bc62c1fbc9339818f5e195
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-15 16:37:38 +0100

    Add notes

--- day-004/lists.py
+++ day-004/lists.py
@@ -4,3 +4,10 @@ print(fruits[0])
 print(fruits[-1])
 
 fruits.extend(["pear", "apricot"])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries",   "Pears", ]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
