8cc5207d34613d18b9a639ebd420d32126ffd270
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-15 17:56:13 +0100

    Finish task

--- day-004/treasure-map.py
+++ day-004/treasure-map.py
@@ -5,5 +5,9 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
+x = int(position[0]) - 1
+y = int(position[1]) - 1
+
+map[x][y] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
