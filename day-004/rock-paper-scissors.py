f3052fec957c2b22051dfd889573ebc95a310da8
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-16 16:38:58 +0100

    Finish project

--- day-004/rock-paper-scissors.py
+++ day-004/rock-paper-scissors.py
@@ -25,5 +25,40 @@ scissors = '''
 ---.__(___)
 '''
 
-choice = input("Enter your choice (Rock, Paper, Scissors): ")
+human_move = input("Enter your choice (Rock, Paper, Scissors): ")
 
+
+
+def print_move(move):
+    if move == "Rock":
+        print(rock)
+    elif move == "Paper":
+        print(paper)
+    elif move == "Scissors":
+        print(scissors)
+    else:
+        print("Unknown move...")
+
+print("You chose:")
+print_move(human_move)
+
+
+moves = ["Rock", "Paper", "Scissors"]
+import random
+computer_move = random.choice(moves)
+
+print("Computer chose:")
+print_move(computer_move)
+
+if human_move == computer_move:
+    print("DRAW")
+    print_move(human_move)
+elif human_move == "Rock" and computer_move == "Scissors":
+    print("YOU WON")
+    print_move(human_move)
+elif human_move == "Paper" and computer_move == "Rock":
+    print("YOU WON")
+    print_move(human_move)
+else:
+    print("YOU LOST")
+    print_move(computer_move)
