4b3934e82ac79829a751bc19e6c5e786f3e64ea1
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-20 19:24:14 +0100

    Done

--- day-005/high-score.py
+++ day-005/high-score.py
@@ -5,4 +5,12 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
+# Proper way
 print(max(student_scores))
+
+# Loopy way
+high_score = 0
+for score in student_scores:
+    if score > high_score:
+        high_score = score
+print(high_score)
