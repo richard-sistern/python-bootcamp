0de5b6577cf88d7fef21741acddb688bf4112fbb
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-21 22:08:31 +0100

    Refactor

--- day-005/adding-evens.py
+++ day-005/adding-evens.py
@@ -3,3 +3,9 @@ for number in range(1, 101):
     if number % 2 == 0:
         total += number
 print(total)
+
+## Refactor
+total = 0
+for number in range(2, 101, 2):
+    total += number
+print(total)
