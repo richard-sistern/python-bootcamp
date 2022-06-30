288d8f6ac4a8ca8ca4b000e6c8a252b782d1d70a
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-30 19:26:54 +0100

    Add maze

---
+++ day-006/maze.py
@@ -0,0 +1,22 @@
+# The functions move() and turn_left().
+# Either the test front_is_clear() or wall_in_front(), right_is_clear() or wall_on_right(), and at_goal().
+# How to use a while loop and if/elif/else statements.
+# It might be useful to know how to use the negation of a test (not in Python).
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
