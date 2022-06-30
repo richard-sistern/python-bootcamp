c57f94a66c4ed636f77d04876657a97669d3d83f
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-30 19:18:36 +0100

    Update

--- day-006/hurdles.py
+++ day-006/hurdles.py
@@ -21,3 +21,29 @@ while not at_goal():
         hurdle()
     else:
         move()
+
+# The position, the height and the number of hurdles changes each time this world is reloaded.
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def hurdle():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        hurdle()
+    else:
+        move()
+    
+    
