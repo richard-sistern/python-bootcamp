abe68b9cc32fa26b807617a7e7c692c0c98bb7d9
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-06-30 19:12:28 +0100

    Add hurdle exercise

---
+++ day-006/hurdles.py
@@ -0,0 +1,23 @@
+# The functions move() and turn_left().
+# The conditions front_is_clear() or wall_in_front(), at_goal(), and their negation.
+# How to use a while loop and an if statement.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def hurdle():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        hurdle()
+    else:
+        move()
