1df04655329ac35fdcde4e3010fc37f4b86f5e48
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-07-06 09:47:09 +0100

    Parking

--- day-007/hangman.py
+++ day-007/hangman.py
@@ -6,6 +6,8 @@
     # - Update hangman
 # - End on word/hangman complete
 
+import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
@@ -14,4 +16,10 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
 guess = input("Pick a letter: ").lower
+
+if guess in chosen_word:
+    print("true")
