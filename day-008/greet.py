2182ab3cec3527ecced24b43ee647640ee68f26a
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-03-25 11:19:05 +0000

    Add positional args

--- day-008/greet.py
+++ day-008/greet.py
@@ -4,3 +4,9 @@ def greet(name):
     print(f"PEW {name}")
 
 greet("Biff")
+
+def greet_with(name, location):
+    print(f"Pew {name}")
+    print(f"At {location}")
+
+greet_with("Marty", "the future")
