f67ac180e89c3619cd22a17f9b7b88e3aec7d504
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-03-29 13:44:23 +0100

    Finish exercise

--- day-009/grading.py
+++ day-009/grading.py
@@ -5,14 +5,28 @@ student_scores = {
   "Draco": 74,
   "Neville": 62,
 }
-# 🚨 Don't change the code above 👆
+# Don't change the code above
 
 #TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
 
+#TODO-2: Write your code below to add the grades to student_grades.
+# Scores 91 - 100: Grade = "Outstanding"
+# Scores 81 - 90: Grade = "Exceeds Expectations"
+# Scores 71 - 80: Grade = "Acceptable"
+# Scores 70 or lower: Grade = "Fail"
+for student in student_scores:
+    if student_scores[student] > 90:
+        grade = "Outstanding"
+    elif student_scores[student] > 80:
+        grade = "Exceeds Expectations"
+    elif student_scores[student] > 70:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
 
-#TODO-2: Write your code below to add the grades to student_grades.👇
-
+    student_grades[student] = grade
     
 
-# 🚨 Don't change the code below 👇
+# Don't change the code below
 print(student_grades)
