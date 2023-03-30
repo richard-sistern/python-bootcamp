2e446122422ca665c71c700d0379022280baf7f1
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-03-30 13:31:32 +0100

    Start exercise

---
+++ day-009/dict-in-list.py
@@ -0,0 +1,24 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+# Do NOT change the code above
+
+# TODO: Write the function that will allow new countries
+#to be added to the travel_log. 
+
+
+
+
+
+# Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
