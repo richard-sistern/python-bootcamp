6a847bd851629aed6f49d77933d5c539d9c784db
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-03 17:01:47 +0100

    Finish exercise

--- day-010/days_in_month.py
+++ day-010/days_in_month.py
@@ -2,19 +2,31 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # JAN..DEC
+
+  days = month_days[month - 1] # Count from 0
+
+  if month == 2:
+    if is_leap(year):
+      days += 1 # Add an extra day
+      return days
+    else:
+      return days    
+  else:
+    return days
   
   
-#🚨 Do NOT change any of the code below 
+  
+# Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
