1baabdc43479244463ec47216ae431b5b757ab43
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-05 09:47:36 +0100

    Finish calc exercise

--- day-010/calc.py
+++ day-010/calc.py
@@ -1,44 +1,43 @@
-# Calculator
+from calc_art import logo
 
-operations = {
-  "+": "add",
-  "-": "subtract",
-  "*": "multiply",
-  "/": "divide"
-}
-
-# Add
 def add(n1, n2):
   return n1 + n2
 
-# Subtract
 def subtract(n1, n2):
   return n1 - n2
 
-# Multiply
 def multiply(n1, n2):
   return n1 * n2
 
-# Divide
 def divide(n1, n2):
   return n1 / n2
 
-from calc_art import logo
-print(logo)
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+  print(logo)
 
-for symbol in operations:
-  print(symbol)
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-operation_symbol = input("Pick an operation from the line above: ")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
 
-num2 = int(input("What's the second number?: "))
-
-if operation_symbol not in operations:
-  print("Invalid operation symbol")
-else:
-  calc_function = operations[operation_symbol]
-  answer = calc_function(num1, num2)
-
-  print(f"{num1} {operation_symbol} {num2} = {answer}") #
+calculator()
