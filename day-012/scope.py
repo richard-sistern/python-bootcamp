81dfe587459c60ce3735aebbc8fc22cdf6d4fd4c
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-07 09:25:53 +0000

    Reference global var in func scope

--- day-012/scope.py
+++ day-012/scope.py
@@ -6,5 +6,13 @@ def increase_enemies():
   enemies = 2 # Local scope
   print(f"enemies inside function: {enemies}")
 
+
+def increase_enemies_global():
+  global enemies
+  enemies +=2 # Local scope
+  print(f"enemies inside function with global: {enemies}")
+
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+increase_enemies_global()
