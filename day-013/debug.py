8596e0f560e79c50aa742aff2d9c95cc093bb089
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-10 08:38:15 +0000

    Doc

--- day-013/debug.py
+++ day-013/debug.py
@@ -21,7 +21,7 @@ if year > 1980 and year < 1994:
 elif year >= 1994:
   print("You are a Gen Z.")
 
-Fix the Errors
+# Fix the Errors
 age = int(input("How old are you? "))
 if age > 18:
     print(f"You can drive at age {age}.")
