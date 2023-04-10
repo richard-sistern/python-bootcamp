d22f8c00c9ff304c0271d071834a09548d83e2c1
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-10 08:23:24 +0000

    Fixed 1 and 2

--- day-013/debug.py
+++ day-013/debug.py
@@ -1,17 +1,18 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+# Describe Problem
+def my_function():
+  for i in range(1, 21): # Range only goes to number below last number
+    if i == 20:
+      print("You got it")
+my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6) Index out of range
+dice_num = randint(0, len(dice_imgs) - 1)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
