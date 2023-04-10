b0fd41a63f66193ecf338baf40f543f335c8692a
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-10 08:46:21 +0000

    Fin leap year

--- day-013/leap_year.py
+++ day-013/leap_year.py
@@ -7,7 +7,7 @@
 
 # Fix the code so that it works and when you hit submit it should pass all the tests.
 
-year = input("Which year do you want to check?")
+year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
   if year % 100 == 0:
