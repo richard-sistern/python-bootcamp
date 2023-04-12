8a0691ec62d97364b97a2e55fcacc704930e52ae
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-12 06:56:16 +0000

    Start challenge

--- day-015/coffee.py
+++ day-015/coffee.py
@@ -30,6 +30,26 @@ resources = {
     "coffee": 100,
 }
 
+money = 0
+
+def print_report(resources, money):
+    """Prints a status report to screen"""
+
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money}")
+
+def check_resources(order):
+    """Returns True if sufficient resources for order, otherwise False"""
+    
+    for item in order:
+        if order[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+        
+    return True
+
 # Requirements
 
 # - Print report
@@ -37,3 +57,18 @@ resources = {
 # - Proccess Coins
 # - Check transaction successful
 # - Make coffee
+
+choice = ""
+
+
+while choice != "off":
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if choice == "report":
+        print_report(resources, money)
+    elif choice == "off":
+        print("Powering off.")
+    elif choice in MENU:
+        print(f"{choice}")
+    else:
+        print("Error creating HOT JAVA LAVA.")
