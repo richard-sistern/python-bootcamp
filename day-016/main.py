5d2a824712c6d0260f3b5afde69f3e51d54cfe38
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-14 10:05:21 +0000

    Tidy

--- day-016/main.py
+++ day-016/main.py
@@ -1,3 +1,12 @@
-from turtle import turtle 
+from turtle import Turtle 
 
-timmy = Turtle() # Classes use Pascal case
+# Fixes following error:
+#  no display name and no $DISPLAY environment variable
+import os
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
+t = Turtle() # Classes use Pascal case
+
+print(t)
