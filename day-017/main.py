1948b1c30acc657fcfdcb75c867a4c2e6481999e
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-04-17 20:01:08 +0000

    Finish example

--- day-017/main.py
+++ day-017/main.py
@@ -1,6 +1,39 @@
 # Day 17: Classes
 
+# Pascal case for the name
 class User:
-    pass
+    def __init__(self, user_id, username)  -> None:
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
 
-user_1 = User()
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("bla", "bloop")
+user_2 = User("bloop", "blip")
+
+print(user_1.username)
+print(user_1.followers)
+print(user_1.following)
+
+print(user_2.username)
+user_1.follow(user_2)
+print(user_2.followers)
+print(user_2.following)
+
+
+class Car:
+    def __init__(self, seats):
+        self.seats = seats
+
+    def enter_race_mode(self):
+        self.seats = 2
+
+my_car = Car(5)
+print(my_car.seats)
+
+my_car.enter_race_mode()
+print(my_car.seats)
