1e99ba5655afddf9e3042de4f37b0149acdd4a07
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-05-06 10:48:33 +0000

    Free resources

--- day-024-file/main.py
+++ day-024-file/main.py
@@ -1,3 +1,10 @@
 file = open("./day-024-file/my_file.txt")
 contents = file.read()
 print(contents)
+
+file.close() # Free resources
+
+# Better way that automatically closes file resource
+with open("./day-024-file/my_file.txt") as file2:
+    contents = file2.read()
+    print(contents)
